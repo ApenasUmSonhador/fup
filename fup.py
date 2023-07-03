1db1e4a931a0aa256be6f5fbdc6b2b887f67fae4
Arthur Vinicius Carneiro Nunes <123214565+ApenasUmSonhador@users.noreply.github.com>
2023-07-03 15:58:49 -0300

    Add files via upload

---
+++ fup.py
@@ -0,0 +1,54 @@
+def preenche_matriz(l,c):
+    matriz = []
+    for i in range(l):
+        linha = []
+        for j in range(c):
+            linha.append(input())
+        matriz.append(linha)
+    return matriz
+
+def sub_matriz(m1,m2):
+    matriz = []
+    for i in range(len(m1)):
+        linha = []
+        for j in range(len(m1[i])):
+            
+            linha.append(m1[i][j] - m1[i][j])
+        matriz.append(linha)
+    return matriz
+
+def sum_matriz(m1,m2):
+    matriz = []
+    for i in range(len(m1)):
+        linha = []
+        for j in range(len(m1[i])):
+            
+            linha.append(m1[i][j] + m1[i][j])
+        matriz.append(linha)
+    return matriz
+
+def mult_matrizes(m1,m2):
+    matriz = []
+    for i in range(len(m1)):
+        linha = []
+        for j in range(len(m2)):
+            termo = 0
+            for k in range(len(m2[i])):
+                termo += m1[i][k] * m2[k][j]
+            linha.append(termo)
+        matriz.append(linha)
+    return matriz
+
+def transpor_matriz(m):
+    matriz = []
+    for i in range(len(m[0])):
+        linha = []
+        for j in range(len(m)):
+            linha.append(m[j][i])
+        matriz.append(linha)
+    return matriz
+
+def confere_simetria(m):
+    if m == transporta_matriz(m):
+        return True
+    return False
